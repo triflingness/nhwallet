39ec6e30e6bbb9fb0d8bd63f130d381a72818549
rlajiun <grace3207@naver.com>
2020-12-13 18:39:10 +0900

    Spring Setting

---
+++ nhWallet/src/main/java/com/triflingness/nhwallet/NhWalletApplication.java
@@ -0,0 +1,13 @@
+package com.triflingness.nhwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhWalletApplication.class, args);
+	}
+
+}
---
+++ nhWallet/src/test/java/com/triflingness/nhwallet/NhWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triflingness.nhwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
